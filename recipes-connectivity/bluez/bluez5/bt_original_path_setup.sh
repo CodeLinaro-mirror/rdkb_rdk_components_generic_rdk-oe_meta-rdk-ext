3af47ab7000f3589d8e1e561213d15230479f92e
Jack O'Gorman <jack.ogorman@sky.uk>
2025-06-18 10:46:32 +0100

    RDKTV-36832: pre-existing file causes bluez start to fail
    Reason for change: if there is a file at /opt/lib/bluetooth it needs to be removed for bluez to start
    Test Procedure: create a file at /opt/lib/bluetooth and then start the bluetooth service
    Risks: Low
    Priority: P1
    Signed-off-by: Jack O'Gorman <jack.ogorman@sky.uk>
    
    (cherry picked from commit c4f7078ac8072c57c06c2a1ea49f9ba091dadccc)
    Change-Id: I96d5618f71232138fb5085e1b5d282d3603bd1f9

--- recipes-connectivity/bluez/bluez5/bt_original_path_setup.sh
+++ recipes-connectivity/bluez/bluez5/bt_original_path_setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+#There shouldn't be a file here - if there is already a regular file here, it needs to be deleted
+if [ "$SOC" = "AMLOGIC" ] ; then
+    if [ -f /opt/lib/bluetooth]; then
+        rm /opt/lib/bluetooth;
+    fi
+fi
+
 /bin/mkdir -p /var/lib/bluetooth
 /bin/mkdir -p /opt/lib/bluetooth
 
